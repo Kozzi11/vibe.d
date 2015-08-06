827de74c39aa7302ea52227a4cb55f7bb382a6d2
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-08-06 18:40:33 +0200

    Add comments to the rest-js example and add trailing newline.

--- examples/rest-js/source/app.d
+++ examples/rest-js/source/app.d
@@ -1,11 +1,17 @@
-import vibe.d;
+import vibe.web.rest;
 
+
+// Defines a simple RESTful API
 interface ITest {
+	// GET /compute_sum?a=...&b=...
 	@method(HTTPMethod.GET)
 	float computeSum(float a, float b);
+
+	// POST /to_console {"text": ...}
 	void postToConsole(string text);
 }
 
+// Local implementation that will be provided by the server
 class Test : ITest {
 	import std.stdio;
 	float computeSum(float a, float b) { return a + b; }
@@ -14,12 +20,24 @@ class Test : ITest {
 
 shared static this()
 {
+	import vibe.core.log : logInfo;
+	import vibe.inet.url : URL;
+	import vibe.http.router : URLRouter;
+	import vibe.http.server : HTTPServerSettings, listenHTTP, staticTemplate;
+
+	// Set up the proper base URL, so that the JavaScript client
+	// will find our REST service
 	auto restsettings = new RestInterfaceSettings;
 	restsettings.baseURL = URL("http://127.0.0.1:8080/");
 
 	auto router = new URLRouter;
+	// Serve the generated JavaScript client at /test.js
 	router.get("/test.js", serveRestJSClient!Test(restsettings));
+	// Serve an example page at /
+	// The page will use the test.js script to issue calls to the
+	// REST service.
 	router.get("/", staticTemplate!"index.dt");
+	// Finally register the REST interface defined above
 	router.registerRestInterface(new Test, restsettings);
 
 	auto settings = new HTTPServerSettings;
